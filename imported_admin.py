35b0f656d32ae0765123666ed8c396a317caacd8
kmankan <malink027@gmail.com>
2020-05-23 17:56:20 +1000

    restructure files

--- imported_admin.py
+++
@@ -1,8 +0,0 @@
-# Exercises from Chapter 9 of Python Crash Course - Classes
-
-from admin import Admin
-
-administrator = Admin("Malin", "Kankanamge", 27, 180, "brown")
-administrator.greet_user()
-administrator.access.show_privileges()
-
--- imported_restaurants.py
+++
@@ -1,9 +0,0 @@
-# Exercises from Chapter 9 of Python Crash Course - Classes
-
-# Exercise 9-10
-from restaurant import Restaurant
-example = Restaurant("Mario's Pizza", "Pizza and Pasta")
-example.describe_restaurant()
-example.open_restaurant()
-
-
